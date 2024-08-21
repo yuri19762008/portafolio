607393f28e64727f5b3e0c0846575af082f45243
Yuri Urzua Lebuy <yuri19762008@hotmail.com>
2024-08-21 13:18:09 -0400

    creacion del portafolio

---
+++ assets/js/scripts.js
@@ -0,0 +1,16 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulario enviado correctamente');
+    // Aquí puedes añadir la lógica para enviar el formulario por AJAX
+});
+document.querySelectorAll('.hidden-section').forEach(function(section) {
+    section.addEventListener('mouseover', function() {
+        this.querySelector('.hidden-content').style.opacity = '1';
+        this.querySelector('.hidden-content').style.visibility = 'visible';
+    });
+
+    section.addEventListener('mouseout', function() {
+        this.querySelector('.hidden-content').style.opacity = '0';
+        this.querySelector('.hidden-content').style.visibility = 'hidden';
+    });
+});
